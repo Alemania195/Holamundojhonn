2455c6b37f633202ceb4264d1aa5079e601d587d
Alemania195 <jbarraganramirez0@gmail.com>
2022-09-20 16:55:43 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hola mundo");
+        final static double PI= 3.14;
+        Private final static double PI = 3.14;
+        Private static int numero1;
+        public static void main ( string[] args) [
+                scanner input = new scanner ( System.in);
+        System.out. println ( "digite un numero");
+        numerous1 = input.nextInt();
+        System.out. println("Resultado: " + PI));
+                ]
+
+    }
+
+}
